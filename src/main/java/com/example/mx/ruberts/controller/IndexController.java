7ba19ed3d881934f349a0d5f68ac9b84053f42a3
JRodriAlarc <jona10131313@gmail.com>
2024-06-07 00:40:10 -0600

    Mostrar Contenido de la Base de Datos en Pantalla

--- src/main/java/com/example/mx/ruberts/controller/IndexController.java
+++ src/main/java/com/example/mx/ruberts/controller/IndexController.java
@@ -1,8 +1,34 @@
 package com.example.mx.ruberts.controller;
 
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import com.example.mx.ruberts.service.PersonaServicio;
+import com.example.mx.ruberts.model.Persona;
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import lombok.Data;
 
 @Component
+@Data
+@ViewScoped
 public class IndexController {
+    
+    @Autowired
+    PersonaServicio personaServicio;
+    private List<Persona> personas;
 
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @PostConstruct
+    public void init(){
+        cargarDatos();
+    }
+
+    public void cargarDatos(){
+        this.personas = personaServicio.listarPersonas();
+        personas.forEach((personas) -> logger.info(personas.toString()));
+    }
 }
---
+++ src/main/java/com/example/mx/ruberts/controller/Persona.java
@@ -0,0 +1,5 @@
+package com.example.mx.ruberts.controller;
+
+public class Persona {
+
+}
