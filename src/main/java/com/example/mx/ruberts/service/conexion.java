f3b2b66391409aca024dfcf9a561004fe534d277
JRodriAlarc <jona10131313@gmail.com>
2024-06-10 17:44:40 -0600

    Archivo para conectarme a MongoDB

---
+++ src/main/java/com/example/mx/ruberts/service/conexion.java
@@ -0,0 +1,35 @@
+package com.example.mx.ruberts.service;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class conexion {
+    public static void main(String[] args) {
+        String connectionString = "mongodb+srv://pemamah951:VT3eN3gzAHuVlqjz@rubertsdenim.ab4rawm.mongodb.net/?retryWrites=true&w=majority&appName=RubertsDenim";
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
